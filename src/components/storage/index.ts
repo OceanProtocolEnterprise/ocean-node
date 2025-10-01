b9ec3abb3691928dd670bdbe6cb7e650c76d1658
alexcos20 <alex.coseru@gmail.com>
2025-10-01 14:22:05 +0300

    add configLog and check errors

--- src/components/storage/index.ts
+++ src/components/storage/index.ts
@@ -65,21 +65,27 @@ export abstract class Storage {
   static getStorageClass(
     file: any,
     config: OceanNodeConfig
-  ): UrlStorage | IpfsStorage | ArweaveStorage | S3Storage {
-    const { type } = file
-    switch (
-      type?.toLowerCase() // case insensitive
-    ) {
-      case FileObjectType.URL:
-        return new UrlStorage(file, config)
-      case FileObjectType.IPFS:
-        return new IpfsStorage(file, config)
-      case FileObjectType.ARWEAVE:
-        return new ArweaveStorage(file, config)
-      case FileObjectType.S3:
-        return new S3Storage(file, config)
-      default:
-        throw new Error(`Invalid storage type: ${type}`)
+  ): UrlStorage | IpfsStorage | ArweaveStorage {
+    if (!file) {
+      throw new Error('Empty file object')
+    }
+    try {
+      const { type } = file
+      switch (
+        type?.toLowerCase() // case insensitive
+      ) {
+        case FileObjectType.URL:
+          return new UrlStorage(file, config)
+        case FileObjectType.IPFS:
+          return new IpfsStorage(file, config)
+        case FileObjectType.ARWEAVE:
+          return new ArweaveStorage(file, config)
+        default:
+          throw new Error(`Invalid storage type: ${type}`)
+      }
+    } catch (err) {
+      console.error('Error in getStorageClass: ', err)
+      throw err
     }
   }
 
