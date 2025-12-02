5a45a8b63a0720bf4545aff365fd3e01c4430978
AdriGeorge <adrian7ko@gmail.com>
2025-12-02 13:19:41 +0200

    fix: escrow

--- src/components/core/utils/escrow.ts
+++ src/components/core/utils/escrow.ts
@@ -16,7 +16,7 @@ export class Escrow {
   // eslint-disable-next-line require-await
   async getEscrowContractAddressForChain(chainId: number): Promise<string | null> {
     const addresses = getOceanArtifactsAdressesByChainId(chainId)
-    if (addresses && addresses.Escrow) return addresses.Escrow
+    if (addresses && addresses.EnterpriseEscrow) return addresses.EnterpriseEscrow
     return null
   }
 
