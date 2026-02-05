7676825529f2006084cdb448ed65f45a65c990e3
AdriGeorge <adrian7ko@gmail.com>
2026-02-05 14:45:52 +0200

    fix: add logs

--- src/components/storage/index.ts
+++ src/components/storage/index.ts
@@ -209,6 +209,8 @@ export class UrlStorage extends Storage {
   async getReadableStream(): Promise<StorageReadable> {
     const input = this.getDownloadUrl()
     const file = this.getFile()
+    CORE_LOGGER.info(`Fetching the file from ${input}`)
+    CORE_LOGGER.debug(`Using headers: ${JSON.stringify(file.headers)}`)
     const response = await axios({
       method: 'get',
       url: input,
