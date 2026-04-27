932c074baa0555ec1751b1eb833d6b55f5f5c35f
AdriGeorge <adrian7ko@gmail.com>
2026-04-27 11:05:22 +0300

    fix: fix

--- src/utils/config/builder.ts
+++ src/utils/config/builder.ts
@@ -9,13 +9,13 @@ import { C2DClusterType } from '../../@types/C2D/C2D.js'
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
+import crypto from 'crypto'
 import { computeCodebaseHash } from '../attestation.js'
 
 import {
   getOceanArtifactsAdresses,
   OCEAN_ARTIFACTS_ADDRESSES_PER_CHAIN
 } from '../address.js'
-import { create256Hash } from '../crypt.js'
 import { CONFIG_LOGGER } from '../logging/common.js'
 import { LOG_LEVELS_STR, GENERIC_EMOJIS } from '../logging/Logger.js'
 import { OceanNodeConfigSchema } from './schemas.js'
@@ -24,6 +24,10 @@ import { fileURLToPath } from 'url'
 import lodash from 'lodash'
 
 let previousConfiguration: OceanNodeConfig = null
+
+function createConfigHash(input: string): string {
+  return '0x' + crypto.createHash('sha256').update(input).digest('hex')
+}
 
 function mapEnvToConfig(
   env: NodeJS.ProcessEnv,
@@ -129,7 +133,7 @@ export function buildC2DClusters(
       for (const theURL of clustersURLS) {
         clusters.push({
           connection: theURL,
-          hash: create256Hash(String(count) + theURL),
+          hash: createConfigHash(String(count) + theURL),
           type: C2DClusterType.OPF_K8
         })
         count += 1
@@ -142,7 +146,7 @@ export function buildC2DClusters(
   if (dockerComputeEnvironments) {
     for (const dockerC2d of dockerComputeEnvironments) {
       if (dockerC2d.socketPath || dockerC2d.host) {
-        const hash = create256Hash(
+        const hash = createConfigHash(
           String(count) +
             JSON.stringify({
               socketPath: dockerC2d.socketPath,
