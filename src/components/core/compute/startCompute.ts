b7ff40437fd47ad8d24f083986d9e1ae19b8eacb
AdriGeorge <adrian7ko@gmail.com>
2026-07-03 13:13:43 +0300

    fix: fix

--- src/components/core/compute/startCompute.ts
+++ src/components/core/compute/startCompute.ts
@@ -513,8 +513,8 @@ export class PaidComputeStartHandler extends CommonComputeHandler {
           }
           result.validOrder = elem.transferTxId
 
-          if (!('meta' in algorithm) && ddo.metadata.type === 'algorithm') {
-            const { entrypoint, image, tag, checksum } = ddo.metadata.algorithm.container
+          if (!('meta' in algorithm) && metadata.type === 'algorithm') {
+            const { entrypoint, image, tag, checksum } = metadata.algorithm.container
             const container = { entrypoint, image, tag, checksum }
             algorithm.meta = {
               language: metadata.algorithm.language,
